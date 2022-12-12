66c3df2b04deca4749411a680cc9848389e67afb
Suyog Joshi <suyogjoshi@Suyogs-MacBook-Pro.local>
2022-12-11 23:18:45 -0800

    valid method added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,11 +18,14 @@ public class Main {
                 System.out.println("Congratulations!!!!!");
                 return;
             } else {
+                System.out.println("ELSE");
                 printBoard(board);
                 Scanner scanner = new Scanner(System.in);
                 int input = scanner.nextInt();
-                playerPosition(board, input);
-                computerPosition(board);
+                if (isValidMove(board)){
+                    playerPosition(board, input);
+                    computerPosition(board);
+                }
             }
         }
     }
@@ -44,10 +49,12 @@ public class Main {
      * @param board : an array of strings
      */
     public static void printBoard(String[] board){
+        List<String> playedPosition = new ArrayList<>();
         System.out.println("");
         for (int i=0; i<board.length; i++){
-            if (board[i] == null) {
+            if (board[i] == null && !playedPosition.contains(board[i])) {
                 board[i] = "" + i;
+                playedPosition.add(board[i]);
             }
         }
         System.out.println("\t\t" + board[0] + " | " + board[1] + " | " + board[2]);
@@ -83,6 +90,17 @@ public class Main {
         } else {
             board[randomNumber] = "O";
         }
+    }
+
+    public static boolean isValidMove(String[] board){
+        for (int i=0; i<board.length; i++){
+            if ((board[i] == "X") || (board[i] == "O")){
+                System.out.println("Invalid move");
+                System.out.println("Please select a place not X or O ");
+                return false;
+            }
+        }
+        return true;
     }
 
 }
